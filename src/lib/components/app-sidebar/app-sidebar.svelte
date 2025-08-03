bc140afe6e60347226e9ebaaf9ff16631f6aa980
Nazif Barassounon <nazifbarassounon@gmail.com>
2025-08-03 17:32:46 +0100

    initialize project

---
+++ src/lib/components/app-sidebar/app-sidebar.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import {
+		BookDownIcon,
+		CircleUserIcon,
+		SettingsIcon,
+		LogOutIcon,
+		BoxesIcon,
+		BoxIcon,
+		UsersIcon
+	} from '@lucide/svelte';
+	import { page } from '$app/state';
+
+	import * as Sidebar from '$lib/components/ui/sidebar';
+
+	const baseUrl = '/app';
+
+	const routes = [
+		{
+			title: 'Répartition des débits par session',
+			url: '/sessions',
+			icon: BoxIcon
+		},
+		{
+			title: "Analyse des délais d'aller-retour",
+			url: '/latency',
+			icon: CircleUserIcon
+		},
+		{
+			title: "Analyse des délais d'acquittement",
+			url: '/acknowledgment',
+			icon: SettingsIcon
+		},
+		{
+			title: 'Analyse des flux simultanés (Streams QUIC)',
+			url: '/streams',
+			icon: SettingsIcon
+		},
+		{
+			title: 'Bande passante par fournisseur télécom (ISP)',
+			url: '/isp',
+			icon: BookDownIcon
+		}
+	];
+</script>
+
+<Sidebar.Root>
+	<Sidebar.Content>
+		<Sidebar.Header>
+			<img src="/bjnet_icon.jpeg" alt="logo" />
+		</Sidebar.Header>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>API</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each routes as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton isActive={page.url.pathname === item.url}>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+</Sidebar.Root>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+	import * as Sidebar from '$lib/components/ui/sidebar';
+	import { AppSidebar } from '$lib/components/app-sidebar';
+
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<main class="mx-auto grid w-full grid-rows-[auto_1fr] gap-2 p-4">
+		<Sidebar.Trigger />
+		<div>
+			{@render children?.()}
+		</div>
+	</main>
+</Sidebar.Provider>
